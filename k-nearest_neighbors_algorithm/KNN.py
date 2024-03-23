4fe01ce6608ee4ce3a3776062f24f15ed32a53a7
Diego de Miranda <diego.demiranda@icloud.com>
2024-03-23 12:22:20 -0300

    code optimization
    I used the pandas replace method to map the values ​​of the 'Quality' column to 0 and 1.
    I used the pandas isnull() function to check for missing values ​​and dropna() to remove rows with missing values.

--- k-nearest_neighbors_algorithm/KNN.py
+++ k-nearest_neighbors_algorithm/KNN.py
@@ -10,45 +10,43 @@ def knn_tests(df, k):
     # Divide os dados em atributos (variáveis independentes) x e rótulos (variável dependente) y.
     x = df.drop(columns=['A_id', 'Quality'])
     y = df['Quality']
+
     # Normaliza os dados no intervalo [0, 1].
     x = (x - x.min()) / (x.max() - x.min())
     normal = x.join(y)
     print("normalized matrix:")
     print(normal)
+
     # Divide o conjunto de dados em conjuntos de treinamento e teste.
     train_size = int((70 / 100) * len(df))
-
     x_train, x_test = x[:train_size], x[train_size:]
     y_train, y_test = y[:train_size], y[train_size:]
+
     # Implementa o algoritmo k-NN para fazer previsões no conjunto de teste.
     predictions = []
     for i in range(len(x_test)):
         distances = np.sqrt(np.sum((x_train - x_test.iloc[i]) ** 2, axis=1))
-
         nearest_neighbors = distances.argsort()[:k]
-
         nearest_labels = y_train.iloc[nearest_neighbors]
-
         most_common_label = nearest_labels.mode()[0]
-
         predictions.append(most_common_label)
+
     # Calcula a matriz de confusão e a precisão da classificação.
     print("Confusion matrix:")
     conf_matrix = confusion_matrix(y_test, predictions)
     print(conf_matrix)
     print("Accuracy:", ((predictions == y_test).mean() * 100), "%")
 
-    # Plotando a matriz de confusão
+    # Plota a matriz de confusão
     plt.figure(figsize=(8, 6))
     sns.heatmap(conf_matrix, annot=True, cmap="Blues", fmt="d")
     plt.title("Confusion Matrix")
-    plt.xlabel("Predicted Labels")
-    plt.ylabel("True Labels")
+    plt.xlabel("Predicted")
+    plt.ylabel("Samples")
     plt.show()
 
     # Retorna as previsões feitas pelo modelo.
     return predictions
-
 
 # Lê um arquivo CSV especificado por file_path usando a biblioteca pandas.
 def read_csv_file(file_path):
@@ -63,19 +61,17 @@ def read_csv_file(file_path):
 
 
 def treat_dataset(df):
-    # Realiza o pré-processamento dos dados: transforma a coluna 'Quality' em valores inteiros: 'good' é
-    # mapeado para 1 e 'bad' é mapeado para 0.
-    for i in range(len(df)):
-        if df.loc[i, 'Quality'] == 'good':
-            df.loc[i, 'Quality'] = 1
-        elif df.loc[i, 'Quality'] == 'bad':
-            df.loc[i, 'Quality'] = 0
+    # Realiza o pré-processamento dos dados.
+    # Mapeamento dos valores 'good' e 'bad' para 1 e 0, respectivamente.
+    df['Quality'] = df['Quality'].replace({'good': 1, 'bad': 0})
 
-    df.info()
-
+    # Remoção de linhas com valores ausentes
     df.dropna(inplace=True)
+
+    # Conversão dos tipos de dados das colunas 'Acidity' e 'Quality'.
     df['Acidity'] = df['Acidity'].astype(float)
     df['Quality'] = df['Quality'].astype(int)
+
     df.info()
 
     return df
@@ -83,7 +79,7 @@ def treat_dataset(df):
 
 # Define o caminho do arquivo CSV contendo os dados das maçãs. Chama a função read_csv_file() para ler os dados
 # do arquivo. Se os dados forem lidos com sucesso, chama a função treat_dataset() para pré-processá-los e, em
-# seguida, chama a função knn_tests() para realizar o teste com o algoritmo k-NN.
+# seguida, chama a função knn_tests() para realizar o teste com o algoritmo KNN.
 def main():
     file_path = 'apple_quality.csv'
     apples = read_csv_file(file_path)
